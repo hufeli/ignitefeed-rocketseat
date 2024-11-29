9dcd8b53734bed6a3c40f0a2a8c740a0f5f6afae
Hudson F <hudsfern@gmail.com>
2024-11-29 09:52:23 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Post from "./Post";
+
+const App = () => {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default App;
